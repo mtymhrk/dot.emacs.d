df513fa475bb70ad9cf56e0c10a1323923164952
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2015-09-15 22:02:14 +0900

    popup-keybindings の使用を止め、which-key を導入

---
+++ config/config-which-key.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; which-key
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(which-key-mode 1)
+;; (which-key-setup-side-window-bottom)
+(which-key-setup-minibuffer)
+
+(custom-set-variables '(which-key-idle-delay 0.5))
+
+(provide 'config-which-key)
