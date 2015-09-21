c0640cead326e28b8121439c6d04ac28ef47088a
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2015-09-21 21:46:55 +0900

    ELPA で管理している elisp を更新

---
+++ elpa/fuzzy-20150729.2037/fuzzy-autoloads.el
@@ -0,0 +1,15 @@
+;;; fuzzy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("fuzzy.el") (22015 49494 320704 813000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; fuzzy-autoloads.el ends here
