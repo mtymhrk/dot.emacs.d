d5c2eee5114cc9f8650631ac5531401e4eaa26ef
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2015-12-10 20:13:08 +0900

    ELPA で管理している elisp を更新

--- elpa/sequential-command-20150408.47/sequential-command-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; sequential-command-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "sequential-command-config" "sequential-command-config.el"
-;;;;;;  (21856 19793 25685 128000))
-;;; Generated autoloads from sequential-command-config.el
-
-(autoload 'sequential-command-setup-keys "sequential-command-config" "\
-Rebind C-a, C-e, M-u, M-c, and M-l to seq-* commands.
-If you use `org-mode', rebind C-a and C-e.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("sequential-command-pkg.el" "sequential-command.el")
-;;;;;;  (21856 19793 138812 884000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sequential-command-autoloads.el ends here
