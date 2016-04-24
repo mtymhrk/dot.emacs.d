a07929192b8ec111d7f20fbbfc929b0fc088c3e3
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2016-04-24 19:50:43 +0900

    emacs -q -l した場合に user-emacs-directory が変更されるよう設定

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 ;;; init.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; emacs -q -lした時に、user-emacs-directoryが変わるように
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 個別設定ファイルのロード
 
