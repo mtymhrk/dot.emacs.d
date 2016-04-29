e9f2a9610ea1ecce691a64b594118f2b2dfe5d4a
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2016-04-29 19:25:41 +0900

    NTEmacs で exec-path-from-shell が動くよう設定

--- config/config-exec-path-from-shell.el
+++ config/config-exec-path-from-shell.el
@@ -8,6 +8,26 @@
 (custom-set-variables
  '(exec-path-from-shell-variables '("PATH" "MANPATH" "INFOPATH")))
 
+
+;; NTEmacs 用設定
+(when (eq system-type 'windows-nt)
+  (defvar my:cygwin-bin-path
+    "D:/lib/gnupack_devel-13.01-2015.05.10/app/cygwin/cygwin/bin/")
+  (setq shell-file-name (concat my:cygwin-bin-path "bash"))
+  (setenv "SHELL" shell-file-name)
+
+  ;; 環境変数 PATH のときのみパスの変換を実行
+  (defun ad-exec-path-from-shell-setenv (orig-fun &rest args)
+    (when (string=  (car args) "PATH")
+      (setf (nth 1 args)
+            (with-temp-buffer
+              (call-process (concat my:cygwin-bin-path "cygpath") nil '(t nil) nil "-amp" (nth 1 args))
+              (unless (bobp)
+                (goto-char (point-min))
+                (buffer-substring-no-properties (point) (line-end-position))))))
+    (apply orig-fun args))
+  (advice-add 'exec-path-from-shell-setenv :around 'ad-exec-path-from-shell-setenv))
+
 (exec-path-from-shell-initialize)
 
 
