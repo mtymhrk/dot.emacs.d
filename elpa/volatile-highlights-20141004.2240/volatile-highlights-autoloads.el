ce1798f6529631a0a57a6349c2a930453541a934
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2016-04-29 22:32:21 +0900

    ELPA で管理している elisp を更新

--- elpa/volatile-highlights-20141004.2240/volatile-highlights-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; volatile-highlights-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("volatile-highlights.el") (21581 42516
-;;;;;;  324265 915000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; volatile-highlights-autoloads.el ends here
