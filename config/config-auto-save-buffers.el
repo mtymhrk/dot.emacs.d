32e6ca1c4fea4112641d8a88b08ddb9f64408fa5
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2016-05-07 08:40:24 +0900

    auto-save-buffers を auto-save-buffers-enhanced に移行

--- config/config-auto-save-buffers.el
+++ config/config-auto-save-buffers.el
@@ -2,14 +2,17 @@
 ;;; auto-save-buffers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'auto-save-buffers)
-(run-with-idle-timer 0.5 t 'auto-save-buffers)
+(require 'auto-save-buffers-enhanced)
 
-  ;;; buffer-file-name が空文字列か、拡張子が .gpg のファイルの場合、
-  ;;; auto-save-buffers を無効にする。
-  ;;; EasyPG と auto-save-buffers の相性がよくないため、.gpg ファイル
-  ;;; を無効しにている。
-(setq auto-save-buffers-exclude-regexp "^\\(.*\\.gpg\\)?$")
+;;; buffer-file-name が空文字列か、拡張子が .gpg のファイルの場合、
+;;; auto-save-buffers を無効にする。
+;;; EasyPG と auto-save-buffers の相性がよくないため、.gpg ファイル
+;;; を無効しにている。
+(setq auto-save-buffers-enhanced-exclude-regexp "^\\(.*\\.gpg\\)?$")
 
+;;; auto-save-buffers の有効/無効を C-x a s で切り替える
+(global-set-key (kbd "C-x a s") 'auto-save-buffers-enhanced-toggle-activity)
+
+(auto-save-buffers-enhanced t)
 
 (provide 'config-auto-save-buffers)
