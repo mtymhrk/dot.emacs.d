e462c2d2ad64e39dbb3d6f9dd21f5a3486cbc214
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2017-10-12 22:43:24 +0900

    config-permanent-scratch.el の内容を elisp として perment-scratch.el を作成

---
+++ elisp/orig/permanent-scratch.el
@@ -0,0 +1,32 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; permanent scratch
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun permanent-scratch:make-scratch (&optional arg)
+  (interactive)
+  (progn
+    ;; "*scratch*" を作成して buffer-list に放り込む
+    (set-buffer (get-buffer-create "*scratch*"))
+    (funcall initial-major-mode)
+    (erase-buffer)
+    (when (and initial-scratch-message (not inhibit-startup-message))
+      (insert initial-scratch-message))
+    (or arg (progn (setq arg 0)
+                   (switch-to-buffer "*scratch*")))
+    (cond ((= arg 0) (message "*scratch* is cleared up."))
+          ((= arg 1) (message "another *scratch* is created")))))
+
+(add-hook 'kill-buffer-query-functions
+          ;; *scratch* バッファで kill-buffer したら内容を消去するだけにする
+          (lambda ()
+            (if (string= "*scratch*" (buffer-name))
+                (progn (permanent-scratch:make-scratch 0) nil)
+              t)))
+
+(add-hook 'after-save-hook
+          ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
+          (lambda ()
+            (unless (member (get-buffer "*scratch*") (buffer-list))
+              (permanent-scratch:make-scratch 1))))
+
+(provide 'permanent-scratch)
