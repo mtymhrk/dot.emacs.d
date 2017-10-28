9a96a6b9b585c8fe2484a8330885528f77a86afb
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2017-10-28 23:42:03 +0900

    multiple-cursors のキーバインドを attach-transient-keymap を使って設定

--- config/config-multiple-cursors.el
+++ config/config-multiple-cursors.el
@@ -4,22 +4,30 @@
 
 (require 'multiple-cursors)
 
-(defvar my-mc/prefix-key "M-M")
-(defvar my-mc/keybinds `((,my-mc/prefix-key . 'mc/mark-next-like-this)
-                         ("n" . 'mc/mark-next-like-this)
-                         ("p" . 'mc/mark-previous-like-this)
-                         ("m" . 'mc/mark-more-like-this-extended)
-                         ("u" . 'mc/unmark-next-like-this)
-                         ("U" . 'mc/unmark-previous-like-this)
-                         ("s" . 'mc/skip-to-next-like-this)
-                         ("S" . 'mc/skip-to-previous-like-this)
-                         ("*" . 'mc/mark-all-like-this)
-                         ("d" . 'mc/mark-all-like-this-dwim)
-                         ("i" . 'mc/insert-numbers)
-                         ("o" . 'mc/sort-regions)
-                         ("O" . 'mc/reverse-regions)))
+(require 'attach-transient-keymap)
 
-(require 'smartrep)
-(smartrep-define-key global-map my-mc/prefix-key my-mc/keybinds)
+(defun my-mc/begin ()
+    (interactive)
+  ; nothing to do
+    )
+
+(define-key keymap-ctrl-meta-space (kbd "m") 'my-mc/begin)
+(define-key keymap-ctrl-meta-space (kbd "C-m") 'mc/mark-all-dwim)
+
+(attach-transientkey:define-keylist
+ my-mc/key-list (("n" "next"         mc/mark-next-like-this)
+                 ("p" "prev"         mc/mark-previous-like-this)
+                 ("m" "more"         mc/mark-more-like-this-extended)
+                 ("u" "unmark"       mc/unmark-next-like-this)
+                 ("U" "unmark(prev)" mc/unmark-previous-like-this)
+                 ("s" "skip"         mc/skip-to-next-like-this)
+                 ("S" "skip(prev)"   mc/skip-to-previous-like-this)
+                 ("*" "all"          mc/mark-all-like-this)
+                 ("d" "all-dwim"     mc/mark-all-like-this-dwim)
+                 ("i" "insert-num"   mc/insert-numbers)
+                 ("o" "sort"         mc/sort-regions)
+                 ("O" "reverse"      mc/reverse-regions)))
+
+(attach-transientkey:attach my-mc/begin my-mc/key-list)
 
 (provide 'config-multiple-cursors)
