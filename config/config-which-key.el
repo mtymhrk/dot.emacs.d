ae87d419eff3bf3c2c4d8e1d219b2018959d7050
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2017-10-28 23:42:03 +0900

    which-key の設定を変更

--- config/config-which-key.el
+++ config/config-which-key.el
@@ -3,9 +3,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (which-key-mode 1)
-;; (which-key-setup-side-window-bottom)
-(which-key-setup-minibuffer)
 
-(custom-set-variables '(which-key-idle-delay 0.5))
+(which-key-setup-side-window-bottom)
+
+(custom-set-variables '(which-key-idle-delay 0.3)
+                      '(which-key-show-transient-maps nil))
 
 (provide 'config-which-key)
