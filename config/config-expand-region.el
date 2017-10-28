58ac6694319f8839990dd6c1da8423b1d450a318
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2017-10-28 23:42:03 +0900

    er/expand-region のコマンドのキーバインドを変更(C-M-SPC r)

--- config/config-expand-region.el
+++ config/config-expand-region.el
@@ -7,14 +7,7 @@
 
 (require 'expand-region)
 
-(defvar my-orig-C-M-SPC-command (global-key-binding (kbd "C-M-SPC")))
-(global-set-key (kbd "C-M-SPC") 'er/expand-region)
-(custom-set-variables '(expand-region-contract-fast-key "M-SPC"))
-
-;; (eval-after-load 'config-extend-kill-region
-;;   '(progn
-;;      (setq kill-ring-save-or-do-something (lambda () (er/expand-region 1)))
-;;      (custom-set-variables '(expand-region-contract-fast-key "W"))
-;;      (global-set-key (kbd "C-M-SPC") my-orig-C-M-SPC-command)))
+(define-key keymap-ctrl-meta-space (kbd "r") 'er/expand-region)
+(custom-set-variables '(expand-region-contract-fast-key "R"))
 
 (provide 'config-expand-region)
