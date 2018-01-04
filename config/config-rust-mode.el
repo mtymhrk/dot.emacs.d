85df19d0b10f39b95fa17043cec3c8484f58d99c
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2018-01-05 00:51:32 +0900

    rust-mode の設定を修正

--- config/config-rust-mode.el
+++ config/config-rust-mode.el
@@ -6,17 +6,19 @@
 (eval-after-load "rust-mode"
   '(setq-default rust-format-on-save t))
 
-;;; rustのファイルを編集するときにracerとflycheckを起動する
-(add-hook 'rust-mode-hook (lambda ()
-                            (racer-mode)
-                            (flycheck-rust-setup)))
+;;; rustのファイルを編集するときにracerを起動する
+(add-hook 'rust-mode-hook #'racer-mode)
+
+;;; rustのファイルを編集するときにflycheckを起動する
+(with-eval-after-load 'flycheck
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+  (add-hook 'rust-mode-hook #'flycheck-mode))
 
 ;;; racerのeldocサポートを使う
 (add-hook 'racer-mode-hook #'eldoc-mode)
 
+;;; racerの補完サポートを使う
 (with-eval-after-load 'company
-  ;;; racerの補完サポートを使う
-  (add-hook 'racer-mode-hook (lambda ()
-                               (company-mode))))
+  (add-hook 'racer-mode-hook #'company-mode))
 
 (provide 'config-rust-mode)
