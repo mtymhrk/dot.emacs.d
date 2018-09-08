8c00fcc3266c29e8c7a228fa1e4ed58fcd629a71
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2018-09-08 23:53:27 +0900

    doom-modeline が勝手に有効にしている eldoc-in-minibuffer-mode を無効化

--- config/config-theme.el
+++ config/config-theme.el
@@ -10,5 +10,10 @@
 
 (use-package doom-modeline
   :defer t
+  :config
+  ;; doom-modeline が eldoc-in-minibuffer-mode を有効にするが、不要なのと、これ
+  ;; を有効にすると eval-expression で C-h が効かなくなるで無効にする
+  (when eldoc-in-minibuffer-mode
+    (eldoc-in-minibuffer-mode -1))
   :hook
   (after-init . doom-modeline-init))
