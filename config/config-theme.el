ca4e70086d41cf5b6d8ae5b634a1806bdd3d1743
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2019-08-03 23:41:09 +0900

    theme を doom-dracula に変更、doom-modeline のファイルパス表示をプロジェクト以降のパスだけ表示するよう変更

--- config/config-theme.el
+++ config/config-theme.el
@@ -6,10 +6,12 @@
 
 (use-package doom-themes
   :config
-  (load-theme 'doom-one t))
+  (load-theme 'doom-dracula t))
 
 (use-package doom-modeline
   :defer t
+  :custom
+  (doom-modeline-buffer-file-name-style 'truncate-with-project)
   :config
   ;; doom-modeline が eldoc-in-minibuffer-mode を有効にするが、不要なのと、これ
   ;; を有効にすると eval-expression で C-h が効かなくなるで無効にする
