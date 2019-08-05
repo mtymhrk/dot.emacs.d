9ad7d564d9da9444f71a10b62201090f3025a175
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2019-08-05 20:48:56 +0900

    ivy-hydra を導入

---
+++ elpa/ivy-hydra-20190731.1602/ivy-hydra-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-hydra" "20190731.1602" "Additional key bindings for Ivy" '((emacs "24.1") (ivy "0.12.0") (hydra "0.13.4")) :commit "d3e4514fd72f217c704ae18afdf711bb9036a04d" :keywords '("convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
