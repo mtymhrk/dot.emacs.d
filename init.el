5658fdc196d71775ce6f35454742bb8ecf71ce47
MOTOYAMA Hiroki <mtymhrk@gmail.com>
2020-03-28 13:29:38 +0900

    migemo の辞書ファイルパスが間違っていたのを修正

--- init.el
+++ init.el
@@ -795,7 +795,7 @@ Flycheck
   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs"))
   ;; migemo-dict のパスを指定
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
   (setq migemo-coding-system 'utf-8-unix)
